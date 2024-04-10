58e7ddba3415cc48d5f9bb92f056e51d7aab2c9c
TrojanB <80817398+TrojanB@users.noreply.github.com>
2024-04-10 23:58:30 +0200

    test
    Testowalem to gowno z innego laptopaa lmao

--- scenes/car.gd
+++ scenes/car.gd
@@ -2,10 +2,11 @@ extends Sprite2D
 
 var speed:float = 5
 var carSprite
-var carFrame = 24
+var carFrame = 5
 
 func _ready():
 	scale.x = 1.9
 	scale.y = 1.9
+	frame = 0
 func _physics_process(delta):
 	position += Vector2(-speed,0)
